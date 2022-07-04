934abf6a8974362f3301aef8a19f40c987a83f9d
Adex93 <dmitriev_alexandr93@mail.ru>
2022-07-04 15:37:00 +0300

    MVP3 created

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,13 +1,18 @@
 package com.example.application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("Микросервис Application успешно запущен");
     }
 
 }
